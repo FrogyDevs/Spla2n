c0011dd99114b6cf9d7cf73816413b18b08c4598
Josh Yuasa <joshyuasa@gmail.com>
2026-08-22 09:08:30 +0200

    player as function

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,22 @@ import './style.css';
 
 const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
-
-ctx.fillStyle = 'lime';
-ctx.fillRect(50, 50, 100, 100);
-
-ctx.fillStyle = 'blue';
-ctx.fillRect(200, 50, 100, 100);
-
 console.log('main.ts loaded');
 
-function Player(this: { name: string; x: number; y: number; color: string }, name: string, x: number, y: number, color: string) {
+function Player(this: { name: string; x: number; y: number; color: string; size: number; draw: () => void }, name: string, x: number, y: number, color: string) {
     this.name = name;
     this.x = x;
     this.y = y;
     this.color = color;
+    this.size = 50;
 
+    this.draw = () => {
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, 50, 50);
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  };
 }
+
+const player1 = new (Player as any)('Player 1', 100, 100, 'red');
+player1.draw();
+const player2 = new (Player as any)('Player 2', 200, 200, 'green');
+player2.draw();
