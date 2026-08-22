240678bf598adc278c7d30e5a06fd25a1057b330
Josh Yuasa <joshyuasa@gmail.com>
2026-08-22 08:58:46 +0200

    adding function for player creation

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,22 @@
-const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+import './style.css';
+
+const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d')!;
+
+ctx.fillStyle = 'lime';
+ctx.fillRect(50, 50, 100, 100);
+
+ctx.fillStyle = 'blue';
+ctx.fillRect(200, 50, 100, 100);
+
+console.log('main.ts loaded');
+
+function Player(this: { name: string; x: number; y: number; color: string }, name: string, x: number, y: number, color: string) {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, 50, 50);
+}
